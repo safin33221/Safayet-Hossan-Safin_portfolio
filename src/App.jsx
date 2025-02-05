4c39f681302fad300485ffc04700512c54b6dbf8
safin33221 <saifn33221@gmail.com>
2025-02-05 21:18:13 +0600

    complement to implement Projects Layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import AboutMe from "./Components/AboutMe"
+import Contact from "./Components/Contact"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+import Projects from "./Components/Projects"
+import Skills from "./Components/Skills"
 
 
 function App() {
 
 
   return (
-    <>
-      <h1 className='text-4xl'>Portfolio</h1>
-    </>
+    <div className="bg-primary place-items-center place-content-center">
+      <div className="bg-hero-pattern">
+        <Navbar/>
+        <Hero/>
+
+      </div>
+      <AboutMe/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+
+    </div>
   )
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+
+
+const Navbar = () => {
+    const navLinks = [
+        {
+            id: "about",
+            title: "About",
+        },
+        {
+            id: "work",
+            title: "Work",
+        },
+        {
+            id: "contact",
+            title: "Contact",
+        },
+    ];
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
