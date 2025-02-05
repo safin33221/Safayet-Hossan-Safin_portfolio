6113609c8a62b827a3062c795a4c10819123d5d9
safin33221 <saifn33221@gmail.com>
2025-02-06 00:21:48 +0600

    completed to implement Skills section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import html from '../assets/tech/html.png'
+import css from '../assets/tech/css.png'
+import javascript from '../assets/tech/javascript.png'
+import reactjs from '../assets/tech/reactjs.png'
+import tailwind from '../assets/tech/tailwind.png'
+import nodejs from '../assets/tech/nodejs.png'
+import mongodb from '../assets/tech/mongodb.png'
+import git from '../assets/tech/git.png'
+import figma from '../assets/tech/figma.png'
+import SectionTile from '../Shared/SectionTile'
 
 const Skills = () => {
+    const skills = [
+        html, css, javascript, reactjs, tailwind, nodejs, mongodb, git, figma
+    ]
     return (
         <div>
-            <h1>Skill section</h1>
+            <SectionTile heading="Skills & Technologies." subheading="My Expertise"/>
+            <div className='flex flex-wrap items-center justify-center gap-7 mx-auto px-12'>
+                {
+                    skills.map(skill => <img className='w-20' src={skill}/>)
+                }
+            </div>
         </div>
     );
 };
