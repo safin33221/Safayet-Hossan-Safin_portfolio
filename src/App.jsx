8db88b532cbe04c6dceedb6a4531b649f08d3fc5
safin33221 <saifn33221@gmail.com>
2025-02-05 21:40:49 +0600

    Navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from "./Components/Projects"
 import Skills from "./Components/Skills"
 
 
+
 function App() {
 
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,54 @@
-
+import { useState } from 'react';
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
     const navLinks = [
+        {
+            id: "home",
+            title: "Home",
+        },
         {
             id: "about",
             title: "About",
         },
         {
-            id: "work",
-            title: "Work",
+            id: "skill",
+            title: "Skill",
+        },
+        {
+            id: "project",
+            title: "Project",
         },
         {
             id: "contact",
             title: "Contact",
         },
     ];
+    const [active, setActive] = useState('')
+
+
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className='px-16 mx-auto py-4'>
+            <div className='flex justify-between items-center'>
+                <div className='flex text-white items-center'>
+                    <img className='w-12 h-12' src={logo} alt="" />
+                    <h1>Safayet Hossan Safin</h1>
+                </div>
+                <div>
+                    <ul className='flex gap-3 '>
+                        {
+                            navLinks?.map((link, idx) => <li
+                                key={idx}
+                                onClick={()=>setActive(link.title)}
+                                className={`${active === link.title ? "text-white" : "text-secondary"}`}
+                            ><a href={`#${link.id}`}>{link.title}</a></li>)
+                        }
+                    </ul>
+                </div>
+                <div>
+                    <button className="btn bg-transparent text-white">Resume</button>
+                </div>
+            </div>
         </div>
     );
 };
