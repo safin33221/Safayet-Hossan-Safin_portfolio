02f26c2f380fbbf324382fd8295ab2b794fdb8e1
safin33221 <safin33221@gmail.com>
2025-06-04 21:57:00 +0600

    Add Loader component for loading state management in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react"
 import AboutMe from "./Components/AboutMe"
 import Contact from "./Components/Contact"
 import Hero from "./Components/Hero"
+import Loader from "./Components/Loader"
 import Navbar from "./Components/Navbar"
 import PrepareToProduction from "./Components/PrepareToProduction"
 import Projects from "./Components/Projects"
@@ -10,20 +12,33 @@ import Skills from "./Components/Skills"
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // 3 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
 
   return (
     <div className="bg-primary max-w-screen-2xl mx-auto  ">
       <div className="bg-hero-pattern ">
-        <Navbar/>
-        <Hero/>
+        <Navbar />
+        <Hero />
 
       </div>
-      
-      <AboutMe/>
-      <PrepareToProduction/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
+
+      <AboutMe />
+      <PrepareToProduction />
+      <Skills />
+      <Projects />
+      <Contact />
 
     </div>
   )
